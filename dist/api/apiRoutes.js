33c52d7e5bd975d4232265f2d0fec738c2a177f6
xxKeefer <xxkeefer.code@gmail.com>
2021-03-27 14:33:16 +1000

    made changes for deployement

--- dist/api/apiRoutes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.router = void 0;
-const express_1 = require("express");
-exports.router = express_1.Router();
-// for testing up status of API
-exports.router.get('/', (req, res) => {
-    res.status(200).json({ message: 'API is active.' });
-});
