ddbadd3a078e2aebd5f743ffa1de2c23bf0ce2a7
xxKeefer <xxkeefer.code@gmail.com>
2021-04-14 17:16:59 +1000

    finished CRUD actions for sections

--- src/controller/guide.ts
+++ src/controller/guide.ts
@@ -1,30 +1,84 @@
 import { Request, Response } from 'express'
-import Respond from '../lib/Respond'
 import Section from '../models/section'
 
 export const createSection = async (
   req: Request,
   res: Response
 ): Promise<Response> => {
-  if (!req?.body) return Respond(res, 400, 'Bad Request.')
+  if (!req?.body) return res.status(400).json({ error: 'Bad Request.' })
   const { title, desc } = req!.body
   try {
     const existingSection = await Section.findOne({ title })
-    if (existingSection) return Respond(res, 409, 'Section already exists.')
+    if (existingSection)
+      return res.status(409).json({ error: 'Section already exists.' })
 
     const newSection = await Section.create({ title, desc })
     return res
       .status(201)
       .json({ _id: newSection._id, title: newSection.title })
   } catch (error) {
-    return Respond(res, 500, 'Ooops, something went wrong.')
+    return res.status(500).json({ error: error.message })
   }
 }
 
-export const readSection = () => {}
+export const readSection = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  if (!req?.body) return res.status(400).json({ error: 'Bad Request.' })
+  const { title } = req!.body
+  try {
+    const section = await Section.findOne({ title })
+    if (!section) return res.status(404).json({ message: 'Section not found.' })
+    return res.status(200).json({ section })
+  } catch (error) {
+    return res.status(500).json({ message: error.message })
+  }
+}
 
-export const allSections = () => {}
+export const allSections = async (res: Response): Promise<Response> => {
+  try {
+    const sections = await Section.find({})
+    return res.status(200).json({ sections })
+  } catch (error) {
+    return res.status(500).json({ error: error.message })
+  }
+}
 
-export const updateSection = () => {}
+export const updateSection = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  if (!req?.body) return res.status(400).json({ error: 'Bad Request.' })
+  const { title, desc } = req!.body
+  const description = !desc ? null : desc
+  try {
+    const sectionUpdate = await Section.findOneAndUpdate(
+      { title },
+      { title, desc: description },
+      { new: true }
+    )
+    //TODO:validation????
+    return res.status(200).json({ sectionUpdate })
+  } catch (error) {
+    return res.status(500).json({ error: error.message })
+  }
+}
 
-export const deleteSection = () => {}
+export const deleteSection = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  if (!req?.body) return res.status(400).json({ error: 'Bad Request.' })
+  const { title } = req!.body
+  try {
+    const sectionDeleted = await Section.findOneAndDelete(
+      { title },
+      { new: true }
+    )
+    //TODO:validation??????
+    return res.status(200).json({ sectionDeleted })
+  } catch (error) {
+    return res.status(500).json({ error: error.message })
+  }
+}
